a4c4a3985e6d63a2e92b2f0937fdbd6522a50f17
vincentchirchir <vincentchirchir273@gmail.com>
2026-03-16 08:46:58 -0600

    more modification on Linear Referencing

--- Assignments/Assign03/GEOS456_Assign03_Chirchir.py
+++ Assignments/Assign03/GEOS456_Assign03_Chirchir.py
@@ -1,4 +1,5 @@
 import arcpy
+from arcpy.sa import *
 
 # setting up workspace
 arcpy.env.overwriteOutput = True
@@ -6,33 +7,39 @@ arcpy.env.workspace = (
     r"C:\GIS SCRIPTS\GEOS456\Assignments\Assign03\Spatial_Decisions.gdb"
 )
 
+
 def messages():
-    print(arcpy.GetMessage(0)) #print only the FIRST geoprocessing message
-    count = arcpy.GetMessageCount()#counts ALL geoprocessing messages for any tool
-    print(arcpy.GetMessage(count-1))#indexes the LAST message from a tool
+    print(arcpy.GetMessage(0))
+    count = arcpy.GetMessageCount()
+    print(arcpy.GetMessage(count - 1))
     print()
+
 
 # Check out extension
 arcpy.CheckOutExtension("Spatial")
 
-##Describe the following dem raster dataset properties:
-##a. Data format
-##b. Cell size
-##c. Coordinate system
+# Describe raster dataset properties
 rastersList = arcpy.ListRasters()
-for rasters in rastersList:
-    desc = arcpy.Describe(rasters)
-    print(f" -{rasters}".title())
+for raster in rastersList:
+    desc = arcpy.Describe(raster)
+    print(f" -{raster}".title())
     print("Data Format: ", desc.format)
     print("Cell Size: ", desc.MeanCellHeight)
     print("Coordinate System: ", desc.spatialReference.name)
     print("")
 
-##Use the dem and geolgrid rasters to assign the following criteria:
-##a. Elevation: 1,000 metres to 1,550 metres
-##b. Slope: <= 18 degrees
-##c. Geology Type: Madison Limestone
-# dem = arcpy.Raster("dem")
-# dem_slope = Slope(dem)
-# dem_slope.save("Slope_DEM")
-# messages()
+# Create slope raster from DEM
+dem = arcpy.Raster("dem")
+dem_slope = Slope(dem)
+dem_slope.save("Slope_DEM")
+messages()
+
+# Elevation: 1,000 metres to 1,550 metres
+dem_criteria = (dem >= 1000) & (dem <= 1500)
+dem_criteria.save("DEM_Criteria")
+
+# Slope: <= 18 degrees
+slope_criteria = dem_slope <= 18
+slope_criteria.save("Slope_Criteria")
+
+# Geology Type: Madison Limestone
